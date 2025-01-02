904689f4abd507b189d3674435146671042b8826
shaimatsri <shay@carwiz.co.il>
2025-01-03 00:51:46 +0200

    Add AudioVisualizer component and integrate into homepage
    - Introduced a new AudioVisualizer component to enhance audio interaction capabilities.
    - Replaced the "Hello World" header in app/page.tsx with the AudioVisualizer, allowing users to visualize audio input and interact with guitar notes.
    - The AudioVisualizer includes features for loading audio files, detecting frequencies, and displaying tuning information, improving the overall functionality of the application.

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import AudioVisualizer from "@/components/audio-visualizer";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <h1>Hello World</h1>
+        <AudioVisualizer />
       </main>
       <footer className={styles.footer}>
       </footer>
